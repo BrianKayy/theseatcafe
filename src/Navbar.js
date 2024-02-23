b1abc9c1b259fcf97e2f75dee9ba4fe67a49cb6b
BrianKayy <Briankasumba40@gmail.com>
2024-02-24 03:20:57 +0400

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import logo from './images/logo.jpg';
-import MyNavbar from './MyNavbar';
 
 const Navbar = () => {
   return (
