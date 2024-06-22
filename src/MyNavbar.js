b8339f997422d3e7a2d4f90d2393f416dd07fd67
BrianKayy <Briankasumba40@gmail.com>
2024-06-22 22:11:27 +0400

    final

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -11,16 +11,16 @@ const MyNavbar = () => {
     <i class="fa-solid fa-right-left"></i>
       <div className='new'  >
         <NavLink to="/Breakfast"    >BREAKFAST</NavLink >
-       <NavLink to="/WinterSpecials"  >DINNER</NavLink > 
        <NavLink to="/Desserts"   >DESSERTS</NavLink > 
        <NavLink to="/HotCoffee"  >HOT COFFEE</NavLink >
+       <NavLink  to="/Juice"   > JUICES </NavLink > 
+       <NavLink  to="/New"   >GATHERING SPECIALS</NavLink >
        <NavLink to="/ColdCoffee"   >COLD DRINKS</NavLink >
        <NavLink to="/FilterCoffee"   >FILTER COFFEE</NavLink >
       <NavLink to="/Frappe"   >FRAPPE</NavLink >
-      <NavLink  to="/Juice"   > JUICE </NavLink > 
       <NavLink  to="/Tea"  >TEA</NavLink >
-     <NavLink  to="/New"   >GATHERING SPECIALS</NavLink >
      <NavLink to="/ColdDrinks"  >SOFT DRINKS</NavLink > 
+     <NavLink to="/WinterSpecials"  >DINNER</NavLink > 
       </div>
       </div></div>
   )
